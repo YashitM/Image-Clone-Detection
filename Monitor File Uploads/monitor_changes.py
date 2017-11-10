6d54466162bd2f80bc22fe8396ef676c46d14a98
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2017-11-11 00:13:52 +0530

    Monitor files Work completed

--- Monitor File Uploads/monitor_changes.py
+++ Monitor File Uploads/monitor_changes.py
@@ -2,28 +2,36 @@ import time, sys
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
-# USAGE: python fileanme.py ./foldername
+sys.path.insert(0,"../Convert PDF to PNG/")
+
+import test_insert
 
 class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*"]
+	patterns = ["*"]
 
 # Print the filename that is added
-    def process(self, event):
-        print event.src_path
+	def process(self, event):
+		file_path = event.src_path
+		# file_name = file_path.split("/")[len(file_path.split("/"))-1]
+		# test_insert.convert(file_name)	
+		test_insert.convert(file_path)
 
-    def on_created(self, event):
-        self.process(event)
+	def on_created(self, event):
+		self.process(event)
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    observer = Observer()
-    observer.schedule(MyHandler(), path=args[0] if args else '.')
-    observer.start()
+	if len(sys.argv) < 2:
+		print "USAGE: python filename.py ./path/to/folder"
+	else:
+		args = sys.argv[1:]
+		observer = Observer()
+		# observer.schedule(MyHandler(), path=args[0] if args else '.')
+		observer.schedule(MyHandler(), path=args[0])
+		observer.start()
+		try:
+			while True:
+				time.sleep(1)
+		except KeyboardInterrupt:
+			observer.stop()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
+		observer.join()
