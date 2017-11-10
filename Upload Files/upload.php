b0ff048de58e75d68f7e66cfcb2cb72f064f063d
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-11 00:38:54 +0530

    Php script updated to segregate files into PDFs, PNGs, Others

--- Upload Files/upload.php
+++ Upload Files/upload.php
@@ -3,9 +3,17 @@
 	if (!empty($_FILES)) {
 		$targetFile = $targetDir.time().'-'. $_FILES['file']['name'];
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-		$mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
-		if (strcmp($mime, 'application/pdf') === 0){
-			move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
-		}
+        $mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
+        if (strcmp($mime, 'application/pdf') === 0){
+            $targetDir = $targetDir."PDFs/";
+		    move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+        }
+        else if (strcmp($mime, 'image/png') === 0){
+            $targetDir = $targetDir."PNGs/";
+		    move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+        } else {
+            $targetDir = $targetDir."Others/";
+            move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+        }
 	}
 ?>
