c25a55742057dc970b284062d96c1235068a9761
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-11 01:01:56 +0530

    Changed upload.php to accept PDFs/PNGs/JPEGs/BMPs/Others

--- Upload Files/upload.php
+++ Upload Files/upload.php
@@ -1,18 +1,23 @@
 <?php
 	$targetDir = 'uploads/';
 	if (!empty($_FILES)) {
-		$targetFile = $targetDir.time().'-'. $_FILES['file']['name'];
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
         $mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
         if (strcmp($mime, 'application/pdf') === 0){
-            $targetDir = $targetDir."PDFs/";
-		    move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+            $targetFile = $targetDir.'PDFs/'.time().'-'. $_FILES['file']['name'];
+            move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
         }
         else if (strcmp($mime, 'image/png') === 0){
-            $targetDir = $targetDir."PNGs/";
-		    move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+            $targetFile = $targetDir.'PNGs/'.time().'-'. $_FILES['file']['name'];
+            move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+        } else if (strcmp($mime, 'image/jpeg') === 0) {
+            $targetFile = $targetDir.'JPEGs/'.time().'-'. $_FILES['file']['name'];
+            move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
+        } else if (strcmp($mime, 'image/bmp/') === 0) {
+            $targetFile = $targetDir.'BMPs/'.time().'-'. $_FILES['file']['name'];
+            move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
         } else {
-            $targetDir = $targetDir."Others/";
+            $targetFile = $targetDir.'Others/'.time().'-'. $_FILES['file']['name'];
             move_uploaded_file($_FILES['file']['tmp_name'],$targetFile);
         }
 	}
