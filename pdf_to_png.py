5fc0a32a1bcfa82e1b7cbfab7764575ee960057a
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2017-11-10 22:42:12 +0530

    Organized Items

--- pdf_to_png.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-image = Image.open("1 Drawing-1.png")
-image = image.resize((800, 532), Image.ANTIALIAS)
-quality_val = 100
-image.save("resized.png", 'PNG', quality=quality_val)
