e35958e8d77fc0917643fff4e17ce7b7deecba86
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-12 00:19:11 +0530

    Updated checkPng script for updating png info in DB

--- Convert PDF to PNG/checkPNGorChangeToPng.py
+++ Convert PDF to PNG/checkPNGorChangeToPng.py
@@ -12,6 +12,20 @@ def checkFile(fileName):
 			changeToPngAndSave(filePath)
 			deleteExisting(filePath)
 
+
+def writeToDb(filePath, myhash):
+	size = os.path.getsize(filePath)
+	import mysql.connector
+	from database import login_info
+	import os
+
+	db = mysql.connector.Connect()
+	cursor=db.cursor()
+
+	cursor.execute("UPDATE tbl_uploads SET file_type = 'png', file_size= " + size + " WHERE hash = " + myhash + ";")
+
+	cursor.close()
+
 def changeToPngAndSave(filePath):
 	im = Image.open(filePath)
 	im.save(filePath.split("/")[-1].split(".")[0]+'.png')
