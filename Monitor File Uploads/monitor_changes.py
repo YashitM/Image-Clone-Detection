b6209e5e4f4787faf75d9e8d576f0cb1d706f3ea
Anubhav Chaudhary <achaudhary997@yahoo.in>
2017-11-25 18:47:07 +0530

    Split is now multitherading (hope so)

--- Monitor File Uploads/monitor_changes.py
+++ Monitor File Uploads/monitor_changes.py
@@ -4,27 +4,35 @@ from watchdog.events import PatternMatchingEventHandler
 import magic
 import pdf_after_processing
 import image_after_processing
+import threading
+from queue import Queue
 
 sys.path.insert(0,"../Check Similar PDF Files/")
 
 import compare_hash
 import split
 
+def worker():
+	while True:
+		item = q.get()
+		split.split_pdfs(item)
+		q.task_done()
+
 def compute_hash(file_name):
 	"""
 	Returns the sha256 hash of the bytes of the file given as argument.
 	"""
- 	import hashlib
- 	BUF_SIZE = 65536
- 	sha256 = hashlib.sha256()
- 
- 	with open(file_name, 'rb') as f:
- 		while True:
- 			data = f.read(BUF_SIZE)
- 			if not data:
- 				break
- 			sha256.update(data)
- 	return sha256.hexdigest()
+	import hashlib
+	BUF_SIZE = 65536
+	sha256 = hashlib.sha256()
+
+	with open(file_name, 'rb') as f:
+		while True:
+			data = f.read(BUF_SIZE)
+			if not data:
+				break
+			sha256.update(data)
+	return sha256.hexdigest()
 
 
 """
@@ -50,16 +58,23 @@ class MyHandler(PatternMatchingEventHandler):
 		elif "pdf" in file_type:
 			# Send it to PDF function
 			pdf_after_processing.handle_pdf(file_path)
-			split.split_pdfs(file_path)
+			# Then send it to the split pdf function
+			# split.split_pdfs(file_path)
+			q.put(file_path)
 	def on_created(self, event):
 		self.process(event)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
-		print "USAGE: python filename.py ./path/to/folder"
+		print ("USAGE: python filename.py ./path/to/folder")
 	else:
 		args = sys.argv[1:]
 		observer = Observer()
+		q = Queue()
+		for i in range(2):
+			t = threading.Thread(target=worker)
+			t.daemon = True
+			t.start()
 		# observer.schedule(MyHandler(), path=args[0] if args else '.')
 		observer.schedule(MyHandler(), path=args[0])
 		observer.start()
